8d1cfb8796fe8ad995d2823b7839b711ca11927c
vaibhavshinde7887 <vshinde7887@gmail.com>
2024-07-23 02:38:19 +0530

    Eamil Integration continue + swagger UI integration + Native queries

--- src/main/java/com/application/demo/book_my_show/repository/TicketRepository.java
+++ src/main/java/com/application/demo/book_my_show/repository/TicketRepository.java
@@ -3,8 +3,16 @@ package com.application.demo.book_my_show.repository;
 import com.application.demo.book_my_show.entity.ShowSeatEntity;
 import com.application.demo.book_my_show.entity.TicketEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface TicketRepository extends JpaRepository<TicketEntity,Integer> {
+
+    // native queries - customization of user queries
+
+    @Query( nativeQuery = true,value = "select * from tickets where total_amount<500")
+    public List<TicketEntity> getTicketsByPriceLessThan500();
 }
